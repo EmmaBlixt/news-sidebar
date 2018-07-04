1394c380b4882250951afd02426b2c32b5056c8a
Emma Blixt <emma.blixt13@gmail.com>
2018-07-04 16:43:45 +0200

    Add news class and widget
    Fetches news from an api
    TODO: Print out data on a template to display in the widget

---
+++ StandoutNews.php
@@ -0,0 +1,106 @@
+<?php
+defined('ABSPATH') or die('No script kiddies please!');
+require_once('includes/setup.php');
+
+class StandoutNews {
+    private $id;
+    private $source;
+    private $title;
+    private $author;
+    private $description;
+    private $url;
+    private $urlToImage;
+    private $publishedAt;
+
+    function __construct()
+    {
+      return $this->init();
+    }
+
+    /**
+    * Initialize the class & add WP hooks
+    */
+    public function init()
+    {
+      $this->standout_news_content();
+    }
+
+    /**
+    * Fetch data from the api
+    * @return object $news that contains all the news
+    */
+    private function standout_fetch_data()
+    {
+        try {
+        $response = wp_remote_get('https://newsapi.org/v2/everything?q=bitcoin&apiKey=7318db02d38d4027b31eb8a830a156d9');
+        $news = json_decode($response['body']);
+
+        } catch (Exception $ex) {
+            $news = null;
+        }
+        return $news;
+    }
+
+    /**
+    * Fetch the news source from the api
+    * @param object[] $news
+    * @return array() $output containing the source info from $news
+    */
+    private function get_source($news)
+    {
+      $output = array();
+
+      if(!empty($news->source)) :
+        $source = get_object_vars($news->source);
+        $output = array(
+            'id'       =>  'Id: '  . $source["id"],
+            'source'   =>  'Source: '  . $source["name"]
+            );
+      else :
+        $output = array('status' => 'Not available');
+      endif;
+
+      return $output;
+    }
+
+    /**
+    * Set the news values
+    * @param object[] $news
+    */
+    private function standout_set_news_values($news)
+    {
+        $this->source = $this->get_source($news);
+        $this->title = $news->title;
+        $this->author = $news->author;
+        $this->description = $news->description;
+        $this->url= $news->url;
+        $this->urlToImage= $news->urlToImage;
+        $this->publishedAt = $news->publishedAt;
+    }
+
+    /**
+    * Get all the news content
+    * @return array() $output containing the news
+    */
+    public function standout_news_content()
+    {
+        $json_response = $this->standout_fetch_data()->articles;
+        $output[] = '';
+        if ($json_response == null) :
+          $output = "<h1>Sorry, I couldn't find the api</h1>";
+        else :
+          foreach ($json_response as $news) :
+            $this->standout_set_news_values($news);
+            $output[] = array(
+                'source'       =>  $this->get_source($news),
+                'title'        =>  $news->title,
+                'author'       =>  $news->author,
+                'url'          =>  $news->url,
+                'urlToImage'   =>  $news->urlToImage,
+                'publishedAt'  =>  $news->publishedAt
+            );
+          endforeach;
+        endif;
+        return $output;
+    }
+}
--- news-sidebar.php
+++ news-sidebar.php
@@ -1,9 +1,91 @@
 <?php
-    /*
-    Plugin Name: News sidebar
-    Plugin URI:
-    description: Show recent news in a sidebar
-    Author: Emma Blixt
-    Author URI: https://standout.se
+/*
+Plugin Name: News sidebar
+Plugin URI:
+description: Show recent news in a sidebar
+Author: Emma Blixt
+Author URI: https://standout.se
+*/
+
+defined('ABSPATH') or die('No script kiddies please!');
+require_once('standoutnews.php');
+
+class Standout_News_Widget extends WP_Widget {
+
+    public function __construct() {
+            parent::__construct(
+            'my_custom_widget',
+            __( 'Standout News Widget', 'text_domain' ),
+            array(
+                'customize_selective_refresh' => true,
+            )
+        );
+    }
+
+    /**
+    * Create widget form for backend settings
+    * @param $instance
     */
-?>
+    public function form($instance)
+    {
+        $defaults = array(
+            'title'    => '',
+        );
+
+        // Parse current settings with defaults
+        extract(wp_parse_args((array) $instance, $defaults)); ?>
+
+        <?php // Widget Title ?>
+            <p>
+                <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php _e( 'Widget Title', 'text_domain' ); ?></label>
+                <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+            </p>
+        <?php
+    }
+
+    /**
+    * Update widget settings
+    * @param $new_instance, $old_instance
+    * @return $intance that contains the user input
+    */
+    public function update($new_instance, $old_instance)
+    {
+        $instance = $old_instance;
+        $instance['title']    = isset($new_instance['title']) ? wp_strip_all_tags($new_instance['title']) : '';
+        return $instance;
+    }
+
+    /**
+    * Display the widget
+    * @param $args, $instance
+    */
+    public function widget($args, $instance)
+    {
+        extract($args );
+
+        // Check the widget options
+        $title = isset( $instance['title'] ) ? apply_filters( 'widget_title', $instance['title'] ) : '';
+
+        // WordPress core before_widget hook (always include )
+        echo $before_widget;
+
+        // Display the widget
+        echo '<div class="widget-text wp_widget_plugin_box">';
+
+        // Display widget title if defined
+        if ($title) :
+            echo $before_title . $title . $after_title;
+        endif;
+        $news = new StandoutNews();
+
+        echo '</div>';
+        echo $after_widget;
+    }
+}
+
+// Register the widget
+function standout_register_news_widget() {
+    register_widget( 'Standout_News_Widget' );
+}
+add_action( 'widgets_init', 'standout_register_news_widget' );
+
