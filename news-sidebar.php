9abe7d3bab2e87bd9424953dfe5e314322c50357
Emma Blixt <emma.blixt13@gmail.com>
2018-07-04 10:11:24 +0200

    Init commit :D

---
+++ news-sidebar.php
@@ -0,0 +1,9 @@
+<?php
+    /*
+    Plugin Name: News sidebar
+    Plugin URI:
+    description: Show recent news in a sidebar
+    Author: Emma Blixt
+    Author URI: https://standout.se
+    */
+?>
