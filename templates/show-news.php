f3088b9d540e10bad24f402f7773e8d9036ed6c0
Emma Blixt <emma.blixt13@gmail.com>
2018-07-10 10:00:21 +0200

    Add several sorting options
    Lets user sort their newsfeed on the widget page
    Now you can sort by country/countries & category

--- templates/show-news.php
+++ templates/show-news.php
@@ -1,27 +1,38 @@
 <?php
+$number_of_news = (new StandoutNews)->get_number_of_news();
+$counter = 0;
+
 $html = '<div class="news-container">';
-foreach ((new StandoutNews)->standout_news_content() as $news) :
-    if (!empty($news)) :
-        $html .= '<div class="news-info">';
-        if (!empty($news['urlToImage'])) :
-            $html .= '<div class="news-image" style="background-image: url(' . $news['urlToImage'] . ');">';
-        endif;
-        $html .= '<div class="news-color-overlay">';
-        $html .= '<a href="' . $news['url'] . '">';
-        $html .= '<h1>' . $news['title'] . '</h1>';
-        $html .= '</a>';
-        $html .= '<p>Published at ' . $news['publishedAt'] . ' by '  . $news['author'] . '</p>';
-        $html .= '<div class="news-content">';
-        $html .= '<p>' . $news['description'] . '</p>';
-        $html .= '<p>Source: ' . $news['source']['source'];
-        $html .= '</div>';
-        $html .= '</div>';
-        if (!empty($news['urlToImage'])) :
+if ((new StandoutNews)->standout_news_content() != null) :
+    foreach ((new StandoutNews)->standout_news_content() as $news) :
+        if (!empty($news)) :
+            $counter++;
+            $html .= '<div class="news-info">';
+            if (!empty($news['urlToImage'])) :
+                $html .= '<div class="news-image" style="background-image: url(' . $news['urlToImage'] . ');">';
+            endif;
+            $html .= '<div class="news-color-overlay">';
+            $html .= '<a href="' . $news['url'] . '">';
+            $html .= '<h1>' . $news['title'] . '</h1>';
+            $html .= '</a>';
+            $html .= '<p>Published at ' . $news['publishedAt'] . ' by '  . $news['author'] . '</p>';
+            $html .= '<div class="news-content">';
+            $html .= '<p>' . $news['description'] . '</p>';
+            $html .= '<p>Source: ' . $news['source']['source'];
             $html .= '</div>';
+            $html .= '</div>';
+            if (!empty($news['urlToImage'])) :
+                $html .= '</div>';
+            endif;
+            $html .= '</div>';
+            if($counter == $number_of_news) :
+              break;
+            endif;
         endif;
-        $html .= '</div>';
-    endif;
-endforeach;
+    endforeach;
+else:
+    $html .= "<h1>Sorry, we didn't find any news.<h1>";
+endif;
 $html .= '</div>';
 
 echo $html;
