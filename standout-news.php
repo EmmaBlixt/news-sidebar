865e9dd15c4b5c5a2e8f915badbb49b1359876cc
Emma Blixt <emma.blixt13@gmail.com>
2018-08-16 10:01:18 +0200

    Fix proper usage of printf

--- standout-news.php
+++ standout-news.php
@@ -93,7 +93,7 @@ class Standout_News_Widget extends WP_Widget {
                         );
                     foreach ($countries as $key => $country) :
                         printf(
-                                '<option value='.$key.'>' . $country . ' </option>', $country
+                                '<option value=%s>%s</option>', $key, $country
                             );
                     endforeach;
                     echo '</select>';
